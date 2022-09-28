493a2e8b1e8fa3252556b150968ce7a3687d2d0a
sameer arshad <sameerarshad7771@gmail.com>
2022-09-28 10:50:17 +0500

    unique constraint error resloved

--- profiles/signals.py
+++ profiles/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import pre_save
+from django.utils.translation import gettext_lazy as _
 from django.dispatch import receiver
 
 from profiles.models.profile_model import Profile
@@ -7,5 +8,9 @@ from core.models.user_model import User
 
 @receiver(pre_save, sender=User)
 def create_profile_of_verified_users(sender, instance, **kwargs):
-    if instance.is_verified and User.objects.filter(pk=instance.pk, is_verified=False).exists():
+    if Profile.objects.filter(user=instance.pk).exists():
+        Profile.objects.filter(user=instance)
+
+    elif instance.is_verified and User.objects.filter(
+        pk=instance.pk, is_verified=False).exists():
         Profile.objects.create(user=instance)
