56b79cf0a576a0ec22ea264f261984fed78c31b0
sameer arshad <sameerarshad7771@gmail.com>
2022-10-01 18:36:42 +0500

    default images

--- profiles/migrations/0001_initial.py
+++ profiles/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('uuid_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='core.uuid')),
                 ('user_gender', models.CharField(choices=[('male', 'Male'), ('female', 'Female')], max_length=10)),
                 ('username', models.CharField(max_length=15)),
-                ('profile_image', models.ImageField(default='profile/mail.png', upload_to=profiles.models.profile_model.profile_photo_path)),
+                ('profile_image', models.ImageField(default='profile/male.png', upload_to=profiles.models.profile_model.profile_photo_path)),
                 ('cover_image', models.ImageField(default='cover/default.png', upload_to=profiles.models.profile_model.cover_photo_path)),
                 ('phone_number', phonenumber_field.modelfields.PhoneNumberField(blank=True, max_length=128, region=None)),
                 ('about', models.TextField()),
