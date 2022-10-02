15483ce2c307a313cb76715c51fd336a86b2743d
sameer arshad <sameerarshad7771@gmail.com>
2022-10-02 10:00:37 +0500

    exceptions updated

--- profiles/serializers/profile_serializer.py
+++ profiles/serializers/profile_serializer.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 from profiles.models.profile_model import Profile
 
@@ -15,7 +15,8 @@ class ProfileSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "user": {"read_only": True},
             "username": {"required": False},
-            "about": {"required": False}
+            "about": {"required": False},
+            "profile_image": {"required": False},
         }
 
     def update(self, instance, validated_data):
@@ -24,16 +25,16 @@ class ProfileSerializer(serializers.ModelSerializer):
         cover_image = validated_data.get("cover_image")
         user_gender = validated_data.get("user_gender")
         if user == instance.user:
-            if profile_image == None:
+            if profile_image is None:
                 if user_gender == Profile.Gender.MALE:
                     validated_data.update(profile_image="profile/male.png")
-                if user_gender == Profile.Gender.FEMALE:
+                elif user_gender == Profile.Gender.FEMALE:
                     validated_data.update(profile_image="profile/female.png")
 
-            if cover_image == None:
+            if cover_image is None:
                 validated_data.update(cover_image="cover/default-cover.png")
 
             return super().update(instance, validated_data)
-        raise serializers.ValidationError(_(
+        raise exceptions.PermissionDenied(_(
             "403 forbidden"
         ))
