98a39782484d87c7fafe3716e30cf89eb7a635e0
sameer arshad <sameerarshad7771@gmail.com>
2022-10-13 22:43:20 +0500

    remove unused code

--- profiles/tests/test_profile_api.py
+++ profiles/tests/test_profile_api.py
@@ -11,7 +11,9 @@ class UserProfileTest(TestCase):
         self.password = "testing321"
 
     def test_verified_user_profile_create(self):
-        user = User.objects.create_user(email=self.email, password=self.password)
+        user = User.objects.create_user(
+            email=self.email, password=self.password
+        )
         user.is_verified = True
         if user.is_verified:
             profile = Profile.objects.create(user=user)
@@ -19,7 +21,9 @@ class UserProfileTest(TestCase):
         self.assertTrue("user is not verified")
 
     def test_unverified_user_profile_create(self):
-        user = User.objects.create_user(email=self.email, password=self.password)
+        user = User.objects.create_user(
+            email=self.email, password=self.password
+        )
         if user.is_verified:
             profile = Profile.objects.create(user=user)
             self.assertTrue(profile)
--- profiles/views/profile_view.py
+++ profiles/views/profile_view.py
@@ -6,12 +6,10 @@ from profiles.models.profile_model import Profile
 
 class ProfileRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = ProfileSerializer
-    permission_classes = (permissions.IsAuthenticated, )
     queryset = Profile.objects.all()
 
 
 class ProfileUpdateAPIView(generics.UpdateAPIView):
     serializer_class = ProfileSerializer
-    permission_classes = (permissions.IsAuthenticated, )
     parser_classes = (parsers.MultiPartParser, )
     queryset = Profile.objects.all()
