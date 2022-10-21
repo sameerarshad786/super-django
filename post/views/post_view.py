4eac737b896b864189118e3c9c4249932b84c3e8
sameer arshad <sameerarshad7771@gmail.com>
2022-10-21 21:50:09 +0500

    order by likes and commenst

--- post/views/post_view.py
+++ post/views/post_view.py
@@ -3,9 +3,22 @@ from rest_framework import generics, parsers
 from core.permissions import IsOwner
 from post.serializers.post_serializer import PostSerializer
 from post.models.post_model import Post
+from post.models.postremark_model import PostRemark
 
 
-class PostAPIView(generics.ListCreateAPIView):
+class PostListAPIView(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        for post in Post.objects.all():
+            if PostRemark.objects.filter(on_post=post.id, popularity="like"):
+                return Post.objects.order_by(
+                    "-postremark", "-comments", "-updated_at", "-created_at"
+                )
+        return Post.objects.order_by("-comments", "-created_at")
+
+
+class PostCreateAPIView(generics.CreateAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
     parser_classes = (parsers.MultiPartParser, )
@@ -21,5 +34,4 @@ class PostUpdateAPIView(generics.UpdateAPIView):
 class PostDestroyAPIView(generics.DestroyAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-    parser_classes = (parsers.MultiPartParser, )
     permission_classes = (IsOwner, )
