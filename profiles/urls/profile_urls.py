7cc9035a04661dc45b0ad566819d4bafb9426b29
sameer arshad <sameerarshad7771@gmail.com>
2022-10-29 19:20:29 +0500

    remove profile or cover image to default

--- profiles/urls/profile_urls.py
+++ profiles/urls/profile_urls.py
@@ -6,5 +6,11 @@ PROFILE_PATTERNS = [
     path("detail/<uuid:pk>/", views.ProfileRetrieveAPIView.as_view(),
          name="profile-retrieve"),
     path("update/<uuid:pk>/", views.ProfileUpdateAPIView.as_view(),
-         name="profile-update")
+         name="profile-update"),
+    path("remove_profile_image/<uuid:pk>/",
+         views.DeleteProfileImageAPIView.as_view(),
+         name="profile-image-delete"),
+    path("remove_cover_image/<uuid:pk>/",
+         views.DeleteCoverImageAPIView.as_view(),
+         name="cover-image-delete")
 ]
