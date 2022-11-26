4a8d2ba85d9277eccd94965050cc2d3cb1f3527d
sameer arshad <sameerarshad7771@gmail.com>
2022-11-26 21:45:14 +0500

    optimize profile-retrieve with django querysets

--- profiles/views/profile_view.py
+++ profiles/views/profile_view.py
@@ -1,4 +1,9 @@
-from rest_framework import generics, parsers
+from django.db.models import F, Value, URLField
+from django.db.models.functions import Concat
+from django.conf import settings
+
+from rest_framework import generics, parsers, status
+from rest_framework.response import Response
 
 from profiles.serializers.profile_serializer import (
     ProfileSerializer, ProfileImageSerializer, CoverImageSerializer
@@ -7,9 +12,29 @@ from profiles.models.profile_model import Profile
 from core.permissions import IsOwner
 
 
+
 class ProfileRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = ProfileSerializer
-    queryset = Profile.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        profile = Profile.objects.filter(id=kwargs["pk"]).annotate(
+            profile_picture=Concat(
+                Value(settings.MEDIA_BUCKET),
+                F("profile_image"),
+                output_field=URLField()
+            ),
+            cover_picture=Concat(
+                Value(settings.MEDIA_BUCKET),
+                F("cover_image"),
+                output_field=URLField()
+            ),
+        ).values(
+            "id", "user_id", "username", "user__email", "profile_picture",
+            "cover_picture", "phone_number", "about", "skills", "education",
+            "current_status", "employment_status", "profession", "location",
+            "created_at", "updated_at"
+        )
+        return Response(profile, status=status.HTTP_200_OK)
 
 
 class ProfileUpdateAPIView(generics.UpdateAPIView):
