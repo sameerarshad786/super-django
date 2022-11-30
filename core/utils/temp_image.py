38c178689bf55a586e07a544733f1fd1e6ddee9a
sameer arshad <sameerarshad7771@gmail.com>
2022-11-30 18:57:57 +0500

    queryset file location changed

---
+++ core/utils/temp_image.py
@@ -0,0 +1,10 @@
+import tempfile
+from PIL import Image
+
+
+def temporary_image():
+    image = Image.new('RGB', (100, 100))
+    tmp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
+    image.save(tmp_file, 'jpeg')
+    tmp_file.seek(0)
+    return tmp_file
