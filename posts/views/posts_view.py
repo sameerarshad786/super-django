6d813347240803cac316c534555fe57dc497fb56
sameerarshad786 <sameerarshad7771@gmail.com>
2023-04-30 19:31:32 +0500

    add user details on comments

--- posts/views/posts_view.py
+++ posts/views/posts_view.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from rest_framework import parsers, generics, status
 from rest_framework.response import Response
 
@@ -5,11 +7,18 @@ from ..models import Posts
 from ..serializers import Postserializer
 from core.permissions import IsOwner
 from ..service import post_popularities, user_commented, total_comment
+from friendship.models import Follow
 
 
 class PostsAPIView(generics.ListAPIView):
     serializer_class = Postserializer
-    queryset = Posts.objects.all().order_by("-created_at")
+
+    def get_queryset(self):
+        followings = Follow.objects.filter(
+            follower=self.request.user).values("followee_id")
+        return Posts.objects.filter(
+            Q(user=self.request.user) | Q(user_id__in=followings)
+        ).order_by("-created_at")
 
     def get(self, request, *args, **kwargs):
         query = self.filter_queryset(self.get_queryset())
