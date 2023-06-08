b14f4f89e67f3bf18482768eb0880235bdd0b4c4
sameerarshad7771@gmail.com <sameerarshad7771@gmail.com>
2023-06-08 12:36:21 +0500

    refactor: migration error and add urlpatterns on message.urls

--- message/migrations/0001_initial.py
+++ message/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('core', '0002_bot'),
+        ('core', '0001_initial'),
     ]
 
     operations = [
