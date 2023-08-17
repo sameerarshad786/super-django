545c895e28a6a274f56e683942d0d1463a9cda6b
sameerarshad7771@gmail.com <sameerarshad7771@gmail.com>
2023-08-17 13:29:46 +0500

    remove scraper, using different project to get scraped products

--- supermarket/urls/product_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from ..views import product_views
-
-
-SCRAPED_PRODUCTS_URLS = [
-    path("products/", product_views.ProductsAPIView.as_view(), name="products"),
-]
--- supermarket/urls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .product_urls import SCRAPED_PRODUCTS_URLS
-
-
-urlpatterns = [
-    path("outside-sources/", include(SCRAPED_PRODUCTS_URLS))
-]
